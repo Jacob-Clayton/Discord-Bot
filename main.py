ba3446a7cc94fb496686928a7ea7154f3b96a782
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2022-11-05 18:32:00 +0000

    Created embeded messages & allow intents

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
-import os
+#Import packages
 import discord
 import requests
 import json
+from discord.ext import commands
+from discord import Intents
 
-client = discord.Client(intents=discord.Intents.default())
+#Create discord client, set prefix and store in variable
+bot = commands.Bot(command_prefix = '!', intents=Intents.all())
+
+#Private discord channel token
+token = ('MTAzODE0NjY1MzcyODgwOTEzMQ.G1HBlF.EHORKKXqGdyNg4bw9H3ZV3Kc_fFmEzNR4SrUXg')
+
+#Bot status messages [Connected - Ready - Resumed - Disconected]
+@bot.event
+async def on_connect():
+  print('Bot has connected to Discord')
+
+@bot.event
+async def on_ready():
+  print('Bot is ready')
+
+@bot.event
+async def on_resume():
+  print('Bot resumed')
+
+@bot.event
+async def on_disconnect():
+  print('Bot has disconnected from Discord')
+
+
+#---Wiki embeded message response in any channel---
+@bot.command(name = 'wiki')
+#Context = whichever channel the message is sent
+async def wiki(context):
+#Embed message creation
+  myEmbed = discord.Embed(
+  title='Ape Gang Wiki', 
+  description='https://wiki.apegang.art/', 
+  colour='2f3136')
+  myEmbed.add_field(name='Updated', value='Nov 5th',inline=False)
+#Send embeded wiki message  
+  await context.message.channel.send(embed=myEmbed)
+
+#Read !wiki and return link
+@bot.event
+async def on_message(message):
+  if message.content == 'hi':
+    general_channel = bot.get_channel(1034442573617573941)
+    await general_channel.send('Hello')
+  await bot.process_commands(message)
+
+#Send embeded message
+@bot.event
+async def on_message(msg):
+  if msg.content == 'help':
+    general_channel = bot.get_channel(1034442573617573941)
+
+    myEmbed = discord.Embed(
+      title='Ape Gang Wiki', 
+      description='https://wiki.apegang.art/',
+      colour='2f3136')
+    myEmbed.add_field(name='Updated', value='Nov 5th',inline=False)
+
+    await general_channel.send(embed=myEmbed)
 
 
 #Get nice quote from api and concatenate response with quote author
@@ -14,30 +73,12 @@ def get_quote():
   return (quote)
 
 
-#Insert private discord channel token
-my_secret = os.environ['token']
-
-
-#Bot logged in and working message
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-
-
-#Read !wiki and return link
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content == '!wiki':
-    await message.channel.send('https://wiki.apegang.art/')
-
-
 #Send inspirational quote when !inspire is typed
-@client.event
+@bot.event
 async def on_message(message):
   if message.content == '!inspire':
     quote = get_quote()
     await message.channel.send(quote)
 
-client.run(os.getenv('token'))
+
+bot.run(token)
